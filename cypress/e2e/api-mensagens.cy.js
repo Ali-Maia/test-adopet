664b6f9281e3165f2e3063d1c4fb8b2231f6b13d
Ali-Maia <aliciaengcomp@gmail.com>
2024-12-17 10:55:08 -0300

    refactor: remove token authorization

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,11 +1,9 @@
 describe('Api Adopet', () => {
-  const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlN2FlYWEzNS1hODFlLTQzYWUtYTU1My0zZWZmMTJjZDM5YTgiLCJhZG9wdGVyTmFtZSI6IkFuaW5oYSBEaWFzIiwiaWF0IjoxNzMxNTIxNzIwLCJleHAiOjE3MzE3ODA5MjB9.UPguObyGzlPXhJkc5a6Ax1lFnwJNeVKlH8gT80fEBFw`
-
   it ('Mensagens da API', () => {
     cy.request({
       method: 'GET',
       url: 'https://adopet-api-i8qu.onrender.com/mensagem/e7aeaa35-a81e-43ae-a553-3eff12cd39a8',
-      headers: {authorization},
+      headers: Cypress.env(),
     }).then((res) => {
       expect(res.status).to.be.equal(200);
       expect(res.body).is.not.empty;
